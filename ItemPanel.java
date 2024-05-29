751a5685d4eabc39665a06aa3f53bbbbc563dede
dai282 <88490090+dai282@users.noreply.github.com>
2024-05-29 14:22:36 +1200

    Update ItemPanel.java
    made abstract + takes in orginalIcon to change image depending on the item

--- ItemPanel.java
+++ ItemPanel.java
@@ -5,12 +5,46 @@
  */
 package main;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 /**
  *
  * @author ADMIN
  */
-public class ItemPanel extends JPanel{
+
+//borrowed from Item class in V1
+public abstract class ItemPanel extends JPanel{
+    
+    protected String name;
+    protected int stat;
+    ImageIcon originalIcon;
+
+    public ItemPanel(String name, int stat, ImageIcon originalIcon) {
+        this.name = name;
+        this.stat = stat;
+        this.originalIcon = originalIcon;
+    }
+
+    public ItemPanel() {
+        this.name = null;
+        this.stat = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getStat() {
+        return this.stat;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStat(int stat) {
+        this.stat = stat;
+    }
     
 }
